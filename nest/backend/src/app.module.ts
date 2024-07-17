2f270a6a53d952e429ab4da74de6a5eec7243701
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-17 13:37:11 +0200

    Dodaj TypeORM za mapiranje baze podataka na objekte

--- nest/backend/src/app.module.ts
+++ nest/backend/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
