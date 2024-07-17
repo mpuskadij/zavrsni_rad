496b8a62efec5cb7fd01fff3e742a7253d63f9d1
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-17 12:17:53 +0200

    Dodaj par jediničnih testova za register funkcije kontrolera USersController

--- nest/backend/src/users/users/users.controller.ts
+++ nest/backend/src/users/users/users.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  async register(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ): Promise<any> {
+    if (!isNaN(Number(username))) {
+      throw new HttpException(
+        'Username should not be a number nad must not be empty!',
+        HttpStatus.NOT_ACCEPTABLE,
+      );
+    }
+
+    if (password.length == 0) {
+      throw new HttpException(
+        'Password must not be empty!',
+        HttpStatus.NOT_ACCEPTABLE,
+      );
+    }
+  }
+}
