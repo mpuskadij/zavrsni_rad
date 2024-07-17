a15244df4f4502a4dbc5c63c9844da436a29452b
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-17 12:18:10 +0200

    Dodaj UsersService

--- nest/backend/src/users/users.module.ts
+++ nest/backend/src/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users-service/users-service';
 
 @Module({
-  controllers: [UsersController]
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
