30ff12e7ef0163ac11ed8c997b761897bac78e19
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-20 12:26:59 +0200

    Dodaj CryptoService u Users modul

--- nest/backend/src/users/users.module.ts
+++ nest/backend/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersService } from './users-service/users-service';
 import { GuardsModule } from '../guards/guards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entities/user/user';
+import { CrpytoModule } from '../crpyto/crpyto.module';
 
 @Module({
-  imports: [GuardsModule, TypeOrmModule.forFeature([User])],
+  imports: [GuardsModule, TypeOrmModule.forFeature([User]), CrpytoModule],
   exports: [TypeOrmModule, UsersService],
   controllers: [UsersController],
   providers: [UsersService],
