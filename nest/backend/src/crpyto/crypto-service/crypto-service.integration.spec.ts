fcf2692acabf253173ead99e5d2cb46cc03c54c6
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-22 14:28:08 +0200

    Dodaj integracijske testove za CrpytoService.compareIfPasswordsMatch

--- nest/backend/src/crpyto/crypto-service/crypto-service.integration.spec.ts
+++ nest/backend/src/crpyto/crypto-service/crypto-service.integration.spec.ts
@@ -17,12 +17,41 @@ describe('CryptoService', () => {
     generator = module.get<HashGenerator>(HashGenerator);
   });
 
-  describe('hashPassword (integration tests)', () => {
+  describe('hashPassword', () => {
     it('should return correct properites of object HashedPasswordData', async () => {
       const password: string = 'fsdf';
       const result: HashedPasswordData = await provider.hashPassword(password);
       expect(result.HashedPassword).not.toEqual(password);
       expect(result.Salt).not.toBeNull();
+    });
+  });
+
+  describe('compareIfPasswordsMatch', () => {
+    it('should return true when passwords match', async () => {
+      const password: string = 'fsdf';
+      const hashedPassword: string = (await provider.hashPassword(password))
+        .HashedPassword;
+
+      const result = await provider.compareIfPasswordsMatch(
+        password,
+        hashedPassword,
+      );
+
+      expect(result).toEqual(true);
+    });
+
+    it('should return false when passwords dont match', async () => {
+      const password: string = 'fsdf';
+      const differentPassword = 'fsdh';
+      const hashedPassword: string = (await provider.hashPassword(password))
+        .HashedPassword;
+
+      const result = await provider.compareIfPasswordsMatch(
+        differentPassword,
+        hashedPassword,
+      );
+
+      expect(result).toEqual(false);
     });
   });
 });
