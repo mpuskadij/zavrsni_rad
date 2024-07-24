0e9ad91a058dca993fd290861cedea796aa05664
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-24 11:21:24 +0200

    Ispravi grešku testa

--- nest/backend/src/authentication/jwt-payload/jwt-payload.spec.ts
+++ nest/backend/src/authentication/jwt-payload/jwt-payload.spec.ts
@@ -2,6 +2,6 @@ import { JwtPayload } from './jwt-payload';
 
 describe('JwtPayload', () => {
   it('should be defined', () => {
-    expect(new JwtPayload()).toBeDefined();
+    expect(new JwtPayload('admin', 1)).toBeDefined();
   });
 });
