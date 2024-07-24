3fae876839babcd8a3102afde8cf2e832c38fc46
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-24 11:28:47 +0200

    Dodaj impor ta Decorators modul kako bi se mogao koristiti Payload modul u Bmi kontroleru

--- nest/backend/src/bmi/bmi.module.ts
+++ nest/backend/src/bmi/bmi.module.ts
@@ -7,6 +7,7 @@ import { AuthenticationModule } from '../authentication/authentication.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { BmiService } from './bmi-service/bmi-service';
+import { DecoratorsModule } from '../decorators/decorators.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { BmiService } from './bmi-service/bmi-service';
     GuardsModule,
     AuthenticationModule,
     JwtModule.register({ secret: process.env.JWT_SECRET }),
+    DecoratorsModule,
   ],
   controllers: [BmiController],
   providers: [JwtGuard, AuthenticationService, BmiService],
--- nest/backend/src/bmi/bmi/bmi.controller.ts
+++ nest/backend/src/bmi/bmi/bmi.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtGuard } from '../../guards/jwt/jwt.guard';
 import { NewBmiEntryGuard } from '../../guards/new-bmi-entry/new-bmi-entry.guard';
+import { Payload } from '../../decorators/payload/payload.decorator';
+import { JwtPayload } from '../../authentication/jwt-payload/jwt-payload';
 
 @Controller('api/bmi')
 export class BmiController {
   @Post()
   @UseGuards(JwtGuard, NewBmiEntryGuard)
-  async createBmiEntry(): Promise<any> {
+  async createBmiEntry(
+    @Body('weight') weight: number,
+    @Body('height') height: number,
+    @Payload() payload: JwtPayload,
+  ): Promise<any> {
     return;
   }
 }
