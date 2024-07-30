6449eb9e095480a152dad51633636a0875abfb56
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-30 12:52:19 +0200

    -dodaj SearchExerciseDto koji će se koristiti za ValidationPipe query parametra u GET /api/exercise

--- nest/backend/src/dtos/dtos.module.ts
+++ nest/backend/src/dtos/dtos.module.ts
@@ -4,10 +4,22 @@ import { EntitiesModule } from '../entities/entities.module';
 import { Bmientry } from '../entities/bmientry/bmientry';
 import { JournalEntryDto } from './journal-entry-dto/journal-entry-dto';
 import { DeleteJournalEntryDto } from './journal-entry-dto/delete-journal-entry-dto';
+import { SearchExerciseDto } from './search-exercise-dto/search-exercise-dto';
 
 @Module({
   imports: [EntitiesModule],
-  exports: [BmiEntryDto, JournalEntryDto, DeleteJournalEntryDto],
-  providers: [BmiEntryDto, Bmientry, JournalEntryDto, DeleteJournalEntryDto],
+  exports: [
+    BmiEntryDto,
+    JournalEntryDto,
+    DeleteJournalEntryDto,
+    SearchExerciseDto,
+  ],
+  providers: [
+    BmiEntryDto,
+    Bmientry,
+    JournalEntryDto,
+    DeleteJournalEntryDto,
+    SearchExerciseDto,
+  ],
 })
 export class DtosModule {}
---
+++ nest/backend/src/dtos/search-exercise-dto/search-exercise-dto.spec.ts
@@ -0,0 +1,7 @@
+import { SearchExerciseDto } from './search-exercise-dto';
+
+describe('SearchExerciseDto', () => {
+  it('should be defined', () => {
+    expect(new SearchExerciseDto()).toBeDefined();
+  });
+});
