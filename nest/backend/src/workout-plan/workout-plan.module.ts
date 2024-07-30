b70de901e43d50c410ed427ce3c388beea27e64b
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-30 13:14:41 +0200

    -dodaj import Dtos modula u workout plan modul

--- nest/backend/src/workout-plan/workout-plan.module.ts
+++ nest/backend/src/workout-plan/workout-plan.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ExerciseController } from './exercise/exercise.controller';
 import { GuardsModule } from '../guards/guards.module';
 import { AuthenticationModule } from '../authentication/authentication.module';
+import { DtosModule } from '../dtos/dtos.module';
 
 @Module({
-  imports: [GuardsModule, AuthenticationModule],
+  imports: [GuardsModule, AuthenticationModule, DtosModule],
   controllers: [ExerciseController],
 })
 export class WorkoutPlanModule {}
