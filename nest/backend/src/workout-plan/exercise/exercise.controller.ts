3963938f26fa221303fd9f0708ab3ccf2e5e5fb5
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-30 16:39:30 +0200

    -dodaj e2e test kad se uspješno vrati u tijelu GET /api/exercise

--- nest/backend/src/workout-plan/exercise/exercise.controller.ts
+++ nest/backend/src/workout-plan/exercise/exercise.controller.ts
@@ -7,13 +7,23 @@ import {
 } from '@nestjs/common';
 import { JwtGuard } from '../../guards/jwt/jwt.guard';
 import { SearchExerciseDto } from '../../dtos/search-exercise-dto/search-exercise-dto';
+import { WgerService } from '../wger-service/wger-service';
+import { WgerExerciseDto } from '../../dtos/wger-variaton-dto/wger-variaton-dto';
 
 @Controller('api/exercise')
 export class ExerciseController {
+  constructor(private wgerService: WgerService) {}
+
   @Get()
   @UseGuards(JwtGuard)
   async getExercise(
     @Query(new ValidationPipe({ transform: true }))
     searchExercise: SearchExerciseDto,
-  ): Promise<any> {}
+  ): Promise<any> {
+    const exercisesFromExternalApi: WgerExerciseDto[] =
+      await this.wgerService.getExercisesBySearchTerm(
+        searchExercise.searchTerm,
+      );
+    return exercisesFromExternalApi;
+  }
 }
--- nest/backend/test/exercise.controller.e2e-spec.ts
+++ nest/backend/test/exercise.controller.e2e-spec.ts
@@ -7,6 +7,9 @@ import { JwtPayload } from '../src/authentication/jwt-payload/jwt-payload';
 import { AuthenticationModule } from '../src/authentication/authentication.module';
 import { JwtGuard } from '../src/guards/jwt/jwt.guard';
 import { GuardsModule } from '../src/guards/guards.module';
+import { WgerExerciseDto } from '../src/dtos/wger-variaton-dto/wger-variaton-dto';
+import { DtosModule } from '../src/dtos/dtos.module';
+import { WgerService } from '../src/workout-plan/wger-service/wger-service';
 
 describe('Exercise controller (e2e)', () => {
   let app: INestApplication;
@@ -15,8 +18,14 @@ describe('Exercise controller (e2e)', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [WorkoutPlanModule, AuthenticationModule, GuardsModule],
+      imports: [
+        WorkoutPlanModule,
+        AuthenticationModule,
+        GuardsModule,
+        DtosModule,
+      ],
       controllers: [ExerciseController],
+      providers: [WgerService],
     })
       .overrideGuard(JwtGuard)
       .useValue(true)
@@ -29,11 +38,21 @@ describe('Exercise controller (e2e)', () => {
 
   describe('GET /api/exercise', () => {
     const path = '/api/exercise';
-    it('should return 400 BAD REQUEST if no query parameters passed', async () => {
+
+    it('should return 400 BAD REQUEST if no query passed', async () => {
       const response = await request(app.getHttpServer())
         .get(path)
         .set('jwtPayload', JSON.stringify(payload));
       expect(response.status).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('should return 200 OK if correct query passed', async () => {
+      const response = await request(app.getHttpServer())
+        .get(path)
+        .set('jwtPayload', JSON.stringify(payload))
+        .query({ searchTerm: '2 Handed Kettlebell Swing' });
+      expect(response.status).toBe(HttpStatus.OK);
+      expect(response.body).toBeInstanceOf(Array<WgerExerciseDto>);
     });
   });
 
