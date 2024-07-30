102ae8fc2957edf8d698517251e28c6dbdbf958e
Marin Puškadija <mpuskadij20@foi.hr>
2024-07-30 12:55:13 +0200

    -dodaj atribut searchTerm u SearchExerciseDto s class validator dekoratorima IsString i MinLength

--- nest/backend/src/dtos/search-exercise-dto/search-exercise-dto.ts
+++ nest/backend/src/dtos/search-exercise-dto/search-exercise-dto.ts
@@ -1,1 +1,7 @@
-export class SearchExerciseDto {}
+import { IsString, MinLength } from 'class-validator';
+
+export class SearchExerciseDto {
+  @IsString({ message: 'Search term must be a string!' })
+  @MinLength(1, { message: 'Search term cannot be empty!' })
+  searchTerm: string;
+}
