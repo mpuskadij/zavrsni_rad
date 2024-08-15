4da5b2a2fe084f5d8a86ca16a248c0f8c76197f6
Marin Puškadija <mpuskadij20@foi.hr>
2024-08-15 10:07:00 +0200

    -dodaj metodu setTime

--- nest/backend/src/admin/virtual-time-service/virtual-time-service.spec.ts
+++ nest/backend/src/admin/virtual-time-service/virtual-time-service.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VirtualTimeService } from './virtual-time-service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 describe('VirtualTimeService (unit tests)', () => {
   let provider: VirtualTimeService;
+  let mockConfigService = { set: jest.fn() };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [VirtualTimeService],
+      imports: [ConfigModule.forRoot()],
+      providers: [
+        VirtualTimeService,
+        { provide: ConfigService, useValue: mockConfigService },
+      ],
     }).compile();
 
     provider = module.get<VirtualTimeService>(VirtualTimeService);
+  });
+
+  describe('setTime', () => {
+    it('should set offset on current time based on given parameter', async () => {
+      await provider.setTime(1);
+
+      expect(mockConfigService.set).toHaveBeenCalled();
+    });
   });
 });
--- nest/backend/src/admin/virtual-time-service/virtual-time-service.ts
+++ nest/backend/src/admin/virtual-time-service/virtual-time-service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class VirtualTimeService {}
+export class VirtualTimeService {
+  constructor(private configService: ConfigService) {}
+  async setTime(offset: number): Promise<void> {
+    this.configService.set('TIME_OFFSET', offset);
+  }
+}
