c8eaf94d127e205c05f54c84e0ac0ef1686c5167
Marin Puškadija <mpuskadij20@foi.hr>
2024-08-17 17:38:34 +0200

    -dodaj helmet modul

--- nest/backend/src/main.ts
+++ nest/backend/src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import 'reflect-metadata';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(helmet());
   app.setGlobalPrefix('api');
   await app.listen(3000);
 }
